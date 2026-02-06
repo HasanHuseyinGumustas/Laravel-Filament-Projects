ef2087d173cb2b90b2fdce3d9e414abd8dd8a37a
hh.gumustas <hasangumustas@gmail.com>
2026-02-06 16:31:49 +0300

    Refactor service provider and update authentication model references; add test events route

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test-events', function() {
+    $allEvents = \App\Models\Event::withoutGlobalScopes()->get();
+    $normalEvents = \App\Models\Event::all();
+
+    return response()->json([
+        'total_without_scopes' => $allEvents->count(),
+        'total_with_scopes' => $normalEvents->count(),
+        'events_without_scopes' => $allEvents,
+        'events_with_scopes' => $normalEvents,
+    ]);
+});
