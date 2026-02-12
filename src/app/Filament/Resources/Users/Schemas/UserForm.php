c15e4df5b4f467b3253279b2e3628007aa315681
hh.gumustas <hasangumustas@gmail.com>
2026-02-12 14:30:00 +0300

    Feature: add policies for Artist, Event, Location, and User management

---
+++ src/app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\CheckboxList;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Select::make('roles')
+                    ->relationship('roles', 'name')
+                    ->multiple()
+                    ->preload()
+                    ->searchable(),
+
+                CheckboxList::make('roles')
+                    ->relationship('roles', 'name')
+                    ->searchable(),
+
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email address')
+                    ->email()
+                    ->required(),
+                DateTimePicker::make('email_verified_at'),
+                TextInput::make('password')
+                    ->password()
+                    ->required(),
+            ]);
+    }
+}
